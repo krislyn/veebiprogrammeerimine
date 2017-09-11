8952f415fece25226f05db7fc10ddd6cfba0fc28
Krislyn Luks <krislyn@users.noreply.github.com>
2017-09-11 10:36:59 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Krislyn Luks</title>
+</head>
+<body>
+<h1>About me</h1>
+<p>I'm pretty boring, but this video sure isn't.</p>
+	<div class="background"></div>
+	<div class="container">
+		
+		<div class="movie-container">
+			<iframe width="800" height="450" src="http://www.youtube.com/embed/BEMaH9Sm3lQ" frameborder="0" class="movie" allowfullscreen></iframe>
+		</div>
+	<p> .. </p>
+	
+	<?php
+	echo "Täna on vastik ilm.";
+	?>
+	</body>
+</html>
